f326caaadcb0a8be115eed117493d254e1a2ec16
Thomas Bocek <tom@tomp2p.net>
2018-02-19 18:35:02 +0100

    added more examples

---
+++ vue-input/src/main/java/ch/hsr/Application.java
@@ -0,0 +1,21 @@
+package ch.hsr;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/spa/{value}")
+    public String list(@PathVariable("value") String value) {
+        return "{\"value\": \""+value.toUpperCase()+"\"}";
+    }
+}
+
