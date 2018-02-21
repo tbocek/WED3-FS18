9be56b9f116c53643e5c92cdf02659fbea65285f
Thomas Bocek <tom@tomp2p.net>
2018-02-21 16:18:42 +0100

    added vue.js exercise1

---
+++ vue-exercise1/src/main/java/ch/hsr/Application.java
@@ -0,0 +1,44 @@
+package ch.hsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    final private static Map<String, String> portfolio = new ConcurrentHashMap<String, String>();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/portfolio")
+    public String create() {
+        UUID uuid = UUID.randomUUID();
+        while(portfolio.containsKey(uuid.toString())) {
+            uuid = UUID.randomUUID();
+        }
+        return "{\"uuid\": \""+uuid.toString()+"\"}";
+    }
+    @GetMapping("/portfolio/{uuid}")
+    public String get(@PathVariable("uuid") String uuid) {
+        String retVal = portfolio.get(uuid);
+        if(retVal == null) {
+            return "[]";
+        }
+        return retVal;
+    }
+    @RequestMapping(value = "/portfolio/{uuid}", method = RequestMethod.POST)
+    //@PostMapping("/portfolio/{uuid}")
+    public void set(HttpEntity<String> httpEntity, @PathVariable("uuid") String uuid) {
+        String json = httpEntity.getBody();
+        portfolio.put(uuid, json);
+    }
+}
+
