97da2c2de45e38a94334808a4417f2b0ff55f369
Thomas Bocek <tom@tomp2p.net>
2018-03-01 07:33:07 +0100

    simplified example

--- vue-component/webpack.config.js
+++ vue-component/webpack.config.js
@@ -1,9 +1,8 @@
-var webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    context: __dirname + '/src', // `__dirname` is root of project and `src` is source
     entry: {
-        app: './main/app.js',
+        app: __dirname + '/src/main/app.js',
     },
     output: {
         path: __dirname + '/src/main/resources/static/dist', // `dist` is the destination
@@ -16,9 +15,7 @@ module.exports = {
             loader: 'vue-loader',
         },]
     },
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        }
-    }
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 };
